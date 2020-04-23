74a4a6d0218761c297280ce891e3976f1c8daad1
Rusbel Bermudez <rossberriv13@gmail.com>
2020-04-23 07:43:36 -0500

    Conocer los conceptos básicos de NodeJS

---
+++ conceptos/monohilo.js
@@ -0,0 +1,16 @@
+console.log('Hola mundo');
+
+let i = 0;
+setInterval(function(){
+    console.log(i)
+    i++
+
+    // if (i === 5){
+    //     console.log('Forzamos error')
+    //     var a = 3 + z;
+    // }
+
+
+}, 1000);
+
+console.log('Segunda Instrucción')
